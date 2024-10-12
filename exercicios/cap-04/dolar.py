24d30b65b4d096cf74a36f89c5d9bc2e4938d7bc
Adriano Saadeh <adrianosaadeh@gmail.com>
2024-10-12 15:36:34 -0300

    aulas 4

---
+++ exercicios/cap-04/dolar.py
@@ -0,0 +1,16 @@
+"""A API https://economia.awesomeapi.com.br/last/USD-BRL é uma interface que fornece informações sobre a última cotação 
+de câmbio entre o dólar americano (USD) e o real brasileiro (BRL). Em outras palavras, ela retorna o valor mais recente 
+de 1 dólar em reais, com base nas taxas de câmbio disponíveis no momento da solicitação."""
+
+import requests
+
+url = "https://economia.awesomeapi.com.br/last/USD-BRL"
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    cotacao = float(data["USDBRL"]["bid"])
+    mensagem = f"U$ 1 dólar corresponde a R$ {cotacao:.2f}"
+    print(mensagem)
+else:
+    print(f"A requisição falhou com o código de status {response.status_code}")
